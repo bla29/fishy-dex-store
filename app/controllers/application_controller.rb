7e0662e6b7a016b80eeadf38ea8572a01ce46e03
Brian La <133820463+bla29@users.noreply.github.com>
2025-03-22 16:37:56 -0700

    cqrf fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  protect_from_forgery with: :null_session, if: -> { request.format.json? }
 end
