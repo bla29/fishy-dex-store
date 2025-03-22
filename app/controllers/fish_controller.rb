1a483b267818f0a775a8193e40cbef85c0782caf
Brian La <133820463+bla29@users.noreply.github.com>
2025-03-21 18:02:02 -0700

    Post endpoint complete

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,4 +1,6 @@
 class FishController < ApplicationController
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  # validates :species, :weight, :picture, presence: true
   def index
     @fish = Fish.all
 
@@ -7,5 +9,22 @@ class FishController < ApplicationController
     else
       render json: { error: "Fish not found" }, status: 404
     end
+  end
+  def create
+    @fish = Fish.new(fish_params)
+
+    if @fish
+      render json: @fish
+      @fish.save
+    else
+      render json: { error: "Fish could not be created." }, status: 422
+    end
+  end
+
+
+  private
+
+  def fish_params
+    params.require(:fish).permit(:species, :weight, :date)
   end
 end
