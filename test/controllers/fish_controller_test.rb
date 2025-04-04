8e59c3c5599191fd589617dd856aa66e274b8fd3
Brian La <133820463+bla29@users.noreply.github.com>
2025-04-03 21:35:07 -0700

    changed assert -> assert_response for tests

--- test/controllers/fish_controller_test.rb
+++ test/controllers/fish_controller_test.rb
@@ -3,21 +3,21 @@ require "test_helper"
 class FishControllerTest < ActionDispatch::IntegrationTest
    test "should get index" do
     get fish_index_url
-    assert :success
+    assert_response :success
    end
 
    test "should post" do
     post fish_index_url, params: { species: Fish.first.species, weight: Fish.first.weight, date: Fish.first.date }, as: :json
-    assert :success
+    assert_response :success
    end
 
    test "should update" do
     patch fish_index_url(Fish.last), params: { species: "Brook Trout", weight: 1.5, date: "12/10/12" }, as: :json
-    assert :success
+    assert_response :success
    end
 
    test "should delete" do
     delete fish_index_url(Fish.last), as: :json
-    assert :success
+    assert_response :success
    end
 end
